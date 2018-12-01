186018efcd9afb9cebbb190671646365fc0ccd1b
Razvan Stan <rastan1@bitbucket.org>
2018-12-01 20:14:33 +0200

    init

---
+++ src/main/java/com/blockchat/BlockchatApplication.java
@@ -0,0 +1,12 @@
+package com.blockchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockchatApplication.class, args);
+	}
+}
